47af4d3d3dfa96d6c888f7c3338bf999cf8bb84e
Andres Garcia <andresgarcia0313@gmail.com>
2018-12-05 12:45:14 -0500

    Inicialización de código y se agrega la detección de objetos de entrada

---
+++ Inicializador.js
@@ -0,0 +1,11 @@
+
+//Validar Ejecución Mostrando Texto 12:37
+alert("Ejecución Iniciada");
+//Almacenar Entradas En Una Colección
+var coleccion = document.getElementsByTagName("input");
+//Mostrar Inputs Detectados En Consola
+//Mostrar Inputs Detectados En Una Solapa Visual
+//Grabador De Acciones
+//Grabar input que va a ingresar datos
+//Grabar datos que se ingreso al anterior input
+//Crear información para ser manejada en un formulario
